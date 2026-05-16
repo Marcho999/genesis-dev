4ecbed71ef3d8705cad1975a0ef02790a6e7a18b
ALEJOPLAYEX <117298794+ALEJOPLAYEX@users.noreply.github.com.>
2026-05-16 17:42:55 -0500

    Cambios en las versiones e instalacion de Tailwind

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar">
+      <h2>Genesis Dev</h2>
+    </nav>
+  </template>
+  
+  <style scoped>
+  .navbar {
+    padding: 1rem;
+    background: #001242;
+    color: white;
+  }
+  </style>
